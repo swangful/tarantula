6d48982ad937ac7f622fffb3f05058ee35b0d216
swangful <swangful@gmail.com>
2015-02-10 10:40:13 -0800

    Reverting changes

--- app/controllers/csv_exports_controller.rb
+++ app/controllers/csv_exports_controller.rb
@@ -20,18 +20,18 @@ class CsvExportsController < ApplicationController
     csv = klass.to_csv(';', "\r\n", :recurse => params[:recursion].to_i,
           :export_without_ids => !params[:export_without_ids].blank?) { records }
     
-    single_csv = klass.to_csv(';', "\r\n", :recurse => params[:recursion].to_i,
-          :export_without_ids => !params[:export_without_ids].blank?) { s.title }
+    # single_csv = klass.to_csv(';', "\r\n", :recurse => params[:recursion].to_i,
+    #       :export_without_ids => !params[:export_without_ids].blank?) { s.title }
 
     #if a user selects single case radio and selects a case
     #then send the user the csv file with the selected case only
-    if :export_type = "single_case"
-      send_data single_csv, 
-                    :filename => "#{params[:export_type]}_export.csv",
-                    :disposition => 'attachment'
-    else
+    # if :export_type = "single_case"
+    #   send_data single_csv, 
+    #                 :filename => "#{params[:export_type]}_export.csv",
+    #                 :disposition => 'attachment'
+    # else
       send_data csv, :filename => "#{params[:export_type]}_export.csv",
                    :disposition => 'attachment'
-    end
+    # end
   end
 end
